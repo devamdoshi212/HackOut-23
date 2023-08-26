f22c32c13f830d724dd67951c4d02f0b8b651014
Dhruv9544 <dhruvshah905@gmail.com>
2023-08-26 10:03:01 +0530

    form complete

--- client/src/components/Fileldform.js
+++ client/src/components/Fileldform.js
@@ -2,30 +2,64 @@ import React from "react";
 
 export const Fileldform = () => {
   return (
-    <div>
-      {/* <p class="text-center">login</p> */}
+    <div className="mt-3">
+      <p class="text-center fw-bold fs-2">Enter Deatail about current Disease</p>
 
       <div class="form-row">
         <div class="form-group col-md-4 offset-md-4">
-          Email:
+          DiseaseName:
           <input type="email" class="form-control" />
           <br></br>
 
         </div>
 
         <div class="form-group col-md-4 offset-md-4">
-          Password:
-          <input type="password" class="form-control" />
+          Description:
+          <textarea className="form-control"></textarea>
           <br></br>
         </div>
 
-        <div class="form-group col-md-6 offset-md-4">
-          <button class="btn btn-primary">Login</button>
+        <div class="form-group col-md-4 offset-md-4">
+          Symptoms:
+          <textarea className="form-control"></textarea>
+          <br></br>
         </div>
 
-        <div class="form-group  col-md-6 offset-md-4">
-          <a routerLink="/signup">signup</a>
+        <div class="form-group col-md-4 offset-md-4">
+         
+           Remedies:
+          <textarea className="form-control" class="form-control" placeholder="Enter remedies and Basic remedies"></textarea>
+          <br></br>
         </div>
+
+        <div class="form-group col-md-4 offset-md-4">
+          Precaution:
+          <textarea className="form-control"></textarea>
+          <br></br>
+        </div>
+        <div class="form-group col-md-4 offset-md-4">
+          Visual-content:
+          <textarea className="form-control" placeholder="provide visual video links here"></textarea>
+          <br></br>
+        </div>
+        <div class="form-group col-md-4 offset-md-4">
+          Emergency-Contact Numbers:
+          <textarea className="form-control" ></textarea>
+          <br></br>
+        </div>
+
+        <div class="form-group col-md-4 offset-md-4">
+          General-Contact Numbers:
+          <textarea className="form-control" ></textarea>
+          <br></br>
+        </div>
+        <div class="form-group col-md-6 offset-md-4">
+          <button class="btn btn-primary">Submit</button>
+        </div>
+
+        {/* <div class="form-group  col-md-6 offset-md-4">
+          <a routerLink="/signup">signup</a>
+        </div> */}
       </div>
     </div>
   );
