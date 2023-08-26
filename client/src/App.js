334b343da0875b22e914afd2a042b4bdda927b37
Devam Doshi <devamdoshi212@gmail.com>
2023-08-26 10:18:09 +0530

    add Fieldform

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
+import { Route } from "react-router-dom/cjs/react-router-dom";
 import { Redirect, Switch } from "react-router-dom/cjs/react-router-dom";
+import MainNavigation from "./components/MainNavigation";
+import Dashboard from "./components/Dashboard";
 import "./App.css";
 
 function App() {
-  return;
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/dashboard/currentdiseases"></Redirect>
+      </Route>
+      <Route path="/dashboard" exact>
+        <Redirect to="/dashboard/currentdiseases"></Redirect>
+      </Route>
+      <Route path="/dashboard/currentdiseases">
+        <Dashboard />
+      </Route>
+      <Route path="/dashboard/pastdiseases">
+        <Dashboard></Dashboard>
+      </Route>
+      <Route path="/admin"></Route>
+      <MainNavigation></MainNavigation>
+    </Switch>
+  );
 }
 export default App;
