bbd7d46f4ac24cf65c61d5a4722d5acda17ba0ba
Devam Doshi <devamdoshi212@gmail.com>
2023-08-26 14:48:08 +0530

    error solved

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 require("./app/config/dbconfig").getDbConnection();
@@ -12,13 +13,15 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.post("/signup", function (req, res) {
+  UserController.signup;
 });
-
-app.post("/signup", UserController.signup);
-app.post("/login", UserController.login);
-app.post("/formdata", FormdataController.formdata);
+app.post("/login", function (req, res) {
+  UserController.login;
+});
+app.post("/formdata", function (req, res) {
+  FormdataController.formdata;
+});
 
 app.listen(9999);
 console.log("server started at 9999");
